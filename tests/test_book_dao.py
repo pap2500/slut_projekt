ed1ae80bf3d04bcf68491c2028088efaed3e9105
Xassii <amanda.ivarsson@telia.com>
2025-03-03 13:36:04 +0100

    Added test for deleted book

--- tests/test_book_dao.py
+++ tests/test_book_dao.py
@@ -5,13 +5,19 @@ from book_dao import BookDAO
 class TestBookDAO:
     def setup_method(self):
         self.databas = BookDAO('books.db')
-        books = [{'title': 'A', 'description': 'D', 'author': 'G'},
-                 {'title': 'B', 'description': 'E', 'author': 'H'},
-                 {'title': 'C', 'description': 'F', 'author': 'I'}]
+        books = [{'title': 'Harry Potter', 'description': 'D', 'author': 'G'},
+                 {'title': 'Percy Jackson', 'description': 'E', 'author': 'H'},
+                 {'title': 'Eldens Hemlighet', 'description': 'F', 'author': 'I'}]
         
         for book in books:
             new_book = Book(book['title'], book['description'], book['author'])
             self.databas.insert_book(new_book)
+    
+    def test_delete_book(self):
+        the_book = self.databas.find_by_title('Eldens Hemlighet')
+        self.databas.delete_book(the_book)
+        the_book = self.databas.find_by_title('Eldens Hemlighet')
+        assert the_book == None
     
     def teardown_method(self):
         self.databas.clear_table()
