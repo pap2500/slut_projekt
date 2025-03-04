51268a074fb78dc1fe851e2815d7b9d6a4be728d
Xassii <amanda.ivarsson@telia.com>
2025-03-04 07:01:34 +0100

    Uppdated names of boocks

--- tests/test_book_dao.py
+++ tests/test_book_dao.py
@@ -5,9 +5,9 @@ from book_dao import BookDAO
 class TestBookDAO:
     def setup_method(self):
         self.databas = BookDAO('books.db')
-        books = [{'title': 'A', 'description': 'D', 'author': 'G'},
-                 {'title': 'B', 'description': 'E', 'author': 'H'},
-                 {'title': 'C', 'description': 'F', 'author': 'I'}]
+        books = [{'title': 'Harry Potter', 'description': 'D', 'author': 'G'},
+                 {'title': 'Percy Jackson', 'description': 'E', 'author': 'H'},
+                 {'title': 'Eldens Hemlighet', 'description': 'F', 'author': 'I'}]
         
         for book in books:
             new_book = Book(book['title'], book['description'], book['author'])
