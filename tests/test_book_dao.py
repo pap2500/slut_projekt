659a232e26953e01dad077010e1633517215d7de
MariaAriza05 <mariafernandaariza@hotmail.es>
2025-03-06 13:29:08 +0100

    klar torsdag

--- tests/test_book_dao.py
+++ tests/test_book_dao.py
@@ -3,29 +3,34 @@ from book import Book
 from book_dao import BookDAO
 
 class TestBookDAO:
-    def setup_method(self):
-        self.databas = BookDAO('books.db')
+
+    @pytest.fixture 
+
+    def create_databas (self):
+        databas = BookDAO ('books.db')
         books = [{'title': 'Harry Potter', 'description': 'D', 'author': 'G'},
                  {'title': 'Percy Jackson', 'description': 'E', 'author': 'H'},
                  {'title': 'Eldens Hemlighet', 'description': 'F', 'author': 'I'}]
-        
+
         for book in books:
             new_book = Book(book['title'], book['description'], book['author'])
-            self.databas.insert_book(new_book)
+            databas.insert_book(new_book)
 
+        yield databas
+        databas.clear_table ()
+        databas.close()
+
+        
+        
 #Metod som kollar att det finns tre böcker
 
-    def test_get_all_books (self): 
-        books = self.databas.get_all_books ()
+    def test_get_all_books (self, create_databas): 
+        books = create_databas.get_all_books ()
         assert 3 == len (books)  #För att verifiera att det är 3 böcker 
         
 #Metod som lägger till en bok och veriferar att det är 4 böcker nu
-    def test_insert_book (self):
+    def test_insert_book (self, create_databas):
         new_book = Book ('The Hunger Games', 'J', 'Suzanne Collins') 
-        self.databas.insert_book (new_book)
-        books = self.databas.get_all_books ()
+        create_databas.insert_book (new_book)
+        books = create_databas.get_all_books()
         assert 4 == len (books)
-    
-    def teardown_method(self):
-        self.databas.clear_table()
-        self.databas.close()
--- tests/test_network_config.py
+++ tests/test_network_config.py
@@ -2,24 +2,32 @@ import pytest
 from network_config_manager import NetworkConfigManager
 
 class TestNetworkCongigManager:
-    
-    def setup_method (self):
-        self.conn = NetworkConfigManager()
-        self.conn.connect ()
-        self.conn.update_hostname('1')
-        self.conn.update_interface_state('down')
-        self.conn.update_response_prefix ('Standard Response')
+  
+    @pytest.fixture
 
-#Metod som kollar att hostname är rätt
+    def connect_method (self):
+        conn = NetworkConfigManager()
+        conn.connect ()
+        conn.update_hostname ('1')
+        conn.update_interface_state('down')
+        conn.update_response_prefix ('Standard Response')
 
-    def test_show_hostname (self):
-        host_name = self.conn.show_hostname()
+        yield conn
+        conn.disconnect()
+
+    #Metod som kollar att hostname är rätt
+
+    def test_show_hostname (self, connect_method):
+        connect_method.show_hostname ()
+        host_name = connect_method.show_hostname ()
         assert host_name == 'hostname: 1'
 
-#Metod som updaterar hostname
 
-    def test_update_hostname (self):
-        self.conn.update_hostname('2')
-        host_name = self.conn.show_hostname()
+    #Metod som updaterar hostname
+
+    def test_update_hostname (self, connect_method):
+        connect_method.update_hostname ('2')
+        host_name = connect_method.show_hostname ()
         assert host_name == 'hostname: 2'
+
     
